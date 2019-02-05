02d7857932544a3ada88c13c766bb812b27e1d62
Max <maxkajiwara@gmail.com>
2019-02-05 15:40:20 -0800

    Set up actions

---
+++ reactapp/src/actions/mapActions.js
@@ -0,0 +1,142 @@
+import axios from 'axios';
+
+//
+axios.defaults.headers.common['Authorization'] = `Token ${process.env.TOKEN}`;
+
+// move
+export const MOVE = 'MOVE';
+export const MOVE_SUCCESS = 'MOVE_SUCCESS';
+export const MOVE_ERROR = 'MOVE_ERROR';
+// take treasure
+export const TAKE_TREASURE = 'TAKE_TREASURE';
+export const TAKE_TREASURE_SUCCESS = 'TAKE_TREASURE_SUCCESS';
+export const TAKE_TREASURE_ERROR = 'TAKE_TREASURE_ERROR';
+// drop treasure
+export const DROP_TREASURE = 'DROP_TREASURE';
+export const DROP_TREASURE_SUCCESS = 'DROP_TREASURE_SUCCESS';
+export const DROP_TREASURE_ERROR = 'DROP_TREASURE_ERROR';
+// sell treasure
+export const SELL_TREASURE = 'SELL_TREASURE';
+export const SELL_TREASURE_SUCCESS = 'SELL_TREASURE_SUCCESS';
+export const SELL_TREASURE_ERROR = 'SELL_TREASURE_ERROR';
+// confirm sale
+export const CONFIRM_SALE = 'CONFIRM_SALE';
+export const CONFIRM_SALE_SUCCESS = 'CONFIRM_SALE_SUCCESS';
+export const CONFIRM_SALE_ERROR = 'CONFIRM_SALE_ERROR';
+// check status
+export const CHECK_STATUS = 'CHECK_STATUS';
+export const CHECK_STATUS_SUCCESS = 'CHECK_STATUS_SUCCESS';
+export const CHECK_STATUS_ERROR = 'CHECK_STATUS_ERROR';
+
+// Test loading messages
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const move = (direction, next_room_id) => {
+	return dispatch => {
+		dispatch({ type: MOVE });
+
+		axios
+			.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', {
+				direction,
+				next_room_id
+			})
+
+			.then(async ({ data }) => {
+				await sleep(1000);
+				dispatch({ type: MOVE_SUCCESS, payload: data });
+			})
+
+			.catch(error => dispatch({ type: MOVE_ERROR, payload: error }));
+	};
+};
+
+export const takeTreasure = name => {
+	return dispatch => {
+		dispatch({ type: TAKE_TREASURE });
+
+		axios
+			.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', {
+				name
+			})
+
+			.then(async ({ data }) => {
+				await sleep(1000);
+				dispatch({ type: TAKE_TREASURE_SUCCESS, payload: data });
+			})
+
+			.catch(error => dispatch({ type: TAKE_TREASURE_ERROR, payload: error }));
+	};
+};
+
+export const dropTreasure = name => {
+	return dispatch => {
+		dispatch({ type: DROP_TREASURE });
+
+		axios
+			.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/', {
+				name
+			})
+
+			.then(async ({ data }) => {
+				await sleep(1000);
+				dispatch({ type: DROP_TREASURE_SUCCESS, payload: data });
+			})
+
+			.catch(error => dispatch({ type: DROP_TREASURE_ERROR, payload: error }));
+	};
+};
+
+export const sellTreasure = name => {
+	return dispatch => {
+		dispatch({ type: SELL_TREASURE });
+
+		axios
+			.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', {
+				name
+			})
+
+			.then(async ({ data }) => {
+				await sleep(1000);
+				dispatch({ type: SELL_TREASURE_SUCCESS, payload: data });
+			})
+
+			.catch(error => dispatch({ type: SELL_TREASURE_ERROR, payload: error }));
+	};
+};
+
+export const confirmSale = name => {
+	return dispatch => {
+		dispatch({ type: CONFIRM_SALE });
+
+		axios
+			.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', {
+				name,
+				confirm: 'no'
+			})
+
+			.then(async ({ data }) => {
+				await sleep(1000);
+				dispatch({ type: CONFIRM_SALE_SUCCESS, payload: data });
+			})
+
+			.catch(error => dispatch({ type: CONFIRM_SALE_ERROR, payload: error }));
+	};
+};
+
+export const checkStatus = () => {
+	return dispatch => {
+		dispatch({ type: CHECK_STATUS });
+
+		axios
+			.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/status/')
+
+			.then(async ({ data }) => {
+				await sleep(1000);
+				dispatch({ type: CHECK_STATUS_SUCCESS, payload: data });
+			})
+
+			.catch(error => dispatch({ type: CHECK_STATUS_ERROR, payload: error }));
+	};
+};
